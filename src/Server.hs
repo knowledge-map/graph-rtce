70ec4a652f7a17013d6c018297c567b5487c9109
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-03-23 14:11:47 +1100

    Use STM.

--- src/Server.hs
+++ src/Server.hs
@@ -15,14 +15,14 @@ import Web.Scotty
 
 import Network.Wai.Handler.WebSockets (intercept)
 import Network.WebSockets
-    (DataMessage(..), PendingConnection, ConnectionException(..), acceptRequest,
-     receiveData, sendTextData)
+    (DataMessage(..), receiveData, sendTextData,
+     PendingConnection, acceptRequest, Connection, ConnectionException(..))
 
 import Control.Monad (void, when, forever)
 import Control.Monad.Trans (liftIO)
 import Control.Exception (handle, fromException)
-import Control.Concurrent.MVar
-    (MVar, newMVar, readMVar, takeMVar, putMVar, modifyMVar_)
+import Control.Concurrent.STM
+    (STM, TVar, newTVar, readTVar, writeTVar, atomically)
 
 import qualified Data.Map as M
 import Data.Text (pack, unpack)
@@ -35,17 +35,18 @@ import Pages.Index
 import Types
 type History = [Edit]
 type Graph = M.Map ID Node
+type Clients = M.Map ID Connection
 
 main :: IO ()
 main = do
     putStrLn "Serving on http://localhost:8000"
-    graphRef <- newMVar M.empty
-    clientsRef <- newMVar []
+    graphV <- atomically $ newGraph
+    clientsV <- atomically $ newClients
     let settings = defaultSettings {
         settingsPort = 8000,
-        settingsIntercept = intercept (handleConnection clientsRef graphRef)
+        settingsIntercept = intercept (handleConnection clientsV graphV)
      }
-    app >>= runSettings settings
+    runSettings settings =<< app
 
 app :: IO Application
 app = scottyApp $ do
@@ -59,13 +60,15 @@ app = scottyApp $ do
         js  f = file f >> setHeader "content-type" "text/javascript"
         css f = file f >> setHeader "content-type" "text/css"
 
-handleConnection clientsRef graphRef pending = do
+{- WebSockets connection handler -}
+
+handleConnection clientsV graphV pending = do
     connection <- acceptRequest pending
-    modifyMVar_ clientsRef (\cs -> return $ connection:cs)
-    handle (catchDisconnect connection) $ forever $ do
+    client <- atomically $ addClient clientsV connection
+    handle (catchDisconnect client) $ forever $ do
         msg <- receiveData connection
         case fromFay msg of
-            Just edit -> handleClientEvent connection clientsRef graphRef edit
+            Just edit -> handleClientEvent client clientsV graphV edit
             Nothing -> return ()
     where
         fromFay x = case decode x of
@@ -74,40 +77,88 @@ handleConnection clientsRef graphRef pending = do
         catchDisconnect client e = case fromException e of
             Just ConnectionClosed -> liftIO $ do
                 putStrLn "Client disconnected"
-                modifyMVar_ clientsRef $ return . filter (/= client)
+                atomically $ removeClient clientsV client
+            _otherwise -> return ()
 
--- Create a new node, and inform the client if there was an ID collision.
-handleClientEvent client clientsRef graphRef (Create node@(Node id content)) = do
-    graph <- takeMVar graphRef
-    (node', graph') <- case M.lookup id graph of
-        Nothing -> return (node, M.insert id node graph)
-        Just _  -> do
-            let id' = (fst . M.findMax $ graph) + 1
-                node' = Node id' content
-            sendTo client $ UpdateID id id'
-            return (node', M.insert id' node' graph)
-    putMVar graphRef graph'
-    broadcast clientsRef $ Create node'
+{- Edit event handlers -}
 
--- Update the node with the given id.
-handleClientEvent _client clientsRef graphRef edit@(UpdateContent id old new) = do
-    graph <- takeMVar graphRef
-    (updated, graph') <- case M.lookup id graph of
-        Nothing -> return (False, graph)
-        Just (Node _ old') -> return $ if old /= old'
-            then (False, graph)
-            else (True, M.adjust (\n -> n { nodeContent = new }) id graph)
-    putMVar graphRef graph'
-    when updated $ broadcast clientsRef edit
+-- Handle an event that creates a new node.
+handleClientEvent client clientsV graphV (Create node@(Node id content)) = do
+    node'@(Node id' content') <- atomically $ insertNode graphV node
+    when (id' /= id) $ do
+        conn <- atomically $ getConnection clientsV client
+        case conn of
+            Nothing -> return ()
+            Just c  -> sendTo c (UpdateID id id')
+    broadcast clientsV (Create node')
 
--- If someone selects a node, let everyone know to update their UI state.
-handleClientEvent _ clientsRef _ edit = broadcast clientsRef edit
+-- Handle node content update events.
+handleClientEvent _client clientsV graphV edit@(UpdateContent id old new) = do
+    updated <- atomically $ updateNodeContent graphV id old new
+    when updated $ broadcast clientsV edit
 
--- Clients should not send other event types.
-handleClientEvent _ _ _ _ = return ()
+-- All other events are just multiplexed to all clients.
+handleClientEvent _ clientsV _ edit = broadcast clientsV edit
 
-sendTo client = sendTextData client . encode . showToFay
+-- Convenience: send an object to a client.
+sendTo conn e = sendTextData conn . encode . showToFay $ e
 
-broadcast ref e = do
-    r <- readMVar ref
-    mapM_ (\cl -> sendTo cl e) r
+-- Convenience: send an object to all clients.
+broadcast clientsV e = do
+    conns <- atomically $ getConnections clientsV
+    mapM_ (flip sendTo e) conns
+
+{- Graph data structure and modification -}
+
+newGraph = newTVar M.empty
+
+insertNode :: TVar Graph -> Node -> STM Node
+insertNode graphV node@(Node id content) = do
+    graph <- readTVar graphV
+    -- Obtain a new node, which is either identical to 'node' if its 'id' does
+    -- not already exist in the graph, or modified with a new id if it does.
+    node' <- case M.lookup id graph of
+        Nothing -> return node
+        Just _ -> let id' = (fst . M.findMax $ graph) + 1
+                   in return (Node id' content)
+    writeTVar graphV $ M.insert (nodeId node') node' graph
+    -- Return the new node so the user can inspect whether its ID was changed.
+    return node'
+
+updateNodeContent :: TVar Graph -> ID -> String -> String -> STM Bool
+updateNodeContent graphV id old new = do
+    graph <- readTVar graphV
+    -- Maybe modify the node, if it exists in the graph and its content matches
+    -- 'old'. If the content doesn't match, the edit is outdated.
+    node' <- case M.lookup id graph of
+        Nothing -> return (Node id old)
+        Just node@(Node _ content) ->
+            if content == old
+                then return (Node id new)
+                else return (Node id old)
+    -- Update the graph if the node was modified, and return status either way.
+    if nodeContent node' /= old
+        then writeTVar graphV (M.insert id node' graph) >> return True
+        else return False
+
+{- Connected client registry -}
+
+newClients = newTVar M.empty
+
+addClient clientsV connection = do
+    clients <- readTVar clientsV
+    let id = if M.null clients
+                then 0
+                else (fst . M.findMax $ clients) + 1
+    writeTVar clientsV $ M.insert id connection clients
+    return id
+
+removeClient clientsV client = do
+    clients <- readTVar clientsV
+    writeTVar clientsV $ M.delete client clients
+
+getConnection clientsV client = return Nothing
+
+getConnections clientsV = do
+    clients <- readTVar clientsV
+    return (M.elems clients)
